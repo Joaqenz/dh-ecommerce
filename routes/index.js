b206d028555682c95622ccb9e3e79b3172c2a077
Joaquin <joaquingarayzabal@outlook.com>
2020-10-15 14:09:26 -0300

    Actualizacion plantilla+carrito

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SportLand' });
 });
 
+router.get('/carrito', function(req, res, next) {
+  res.render('carrito', { title: 'SportLand' });
+});
+
+
 module.exports = router;
