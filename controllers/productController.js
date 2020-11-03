886e0836fcbfcabb50c779bb53d73c7635a277e7
Joaquin <joaquingarayzabal@outlook.com>
2020-11-03 19:36:03 -0300

    joaqenz

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
-let dataProducts = JSON.parse(fs.readFileSync('./database/listado.json', 'utf-8'));
+const dataProducts = JSON.parse(fs.readFileSync('./database/listado.json', 'utf-8'));
+const title = 'SportLand'
 
 const productController = {
     main: function (req, res, next){
         res.render('Funciona', {items: dataProducts})
     },
     id: function (req, res, next){
-        res.render('product', {items: dataProducts})
+        let idProduct = req.params.id-1
+        res.render('product', { title, items: dataProducts, idProduct})
     },
     carrito: function (req, res, next){
         res.render('carrito', { title: 'SportLand' })
