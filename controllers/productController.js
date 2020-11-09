ea210064aa0e4a1efe5c4189dca893925d4d1a27
Joaquin Garayzabal <joaquingarayzabal@outlook.com>
2020-11-09 12:37:33 -0300

    productos en linea

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,37 @@
 const fs = require('fs');
 const dataProducts = JSON.parse(fs.readFileSync('./database/listado.json', 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g,".");
 const title = 'SportLand'
 
 const productController = {
     main: function (req, res, next){
         res.render('Funciona', {items: dataProducts})
     },
-    id: function (req, res, next){
-        let idProduct = req.params.id-1
-        res.render('product', {title, items: dataProducts, idProduct})
-    },
     add: function (req, res, next){
         res.render('addProduct', {title})
     },
-
     carrito: function (req, res, next){
         res.render('carrito', {title})
+    },
+    ver:function(req,res,next){
+        var productId = req.params.id;
+        var producto = dataProducts.filter(function(producto){
+            //condicion true/false
+            return productId == producto.id;
+        });
+        console.log(producto);
+        if(producto.length > 0){
+            res.render("product",{
+                producto:producto[0],
+                toThousand,
+                title
+            });
+        }else{
+            res.send("No hay producto con ese id");
+        }
+    },
+    products: function (req, res, next){
+        res.render('products', {title})
     }
 }
 module.exports = (productController)
--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var productController = require('../controllers/productController')
 
 /* GET products listing. */
-router.get('/', productController.main);
+router.get('/', productController.products);
 router.get('/addproduct', productController.add);
 router.get('/carrito', productController.carrito);
-router.get('/:id', productController.id);
+router.get('/:id', productController.ver);
+
 
 module.exports = router;
 
