6ee2f936a181388b811cd2467bf817af203c5b70
Joaquin <joaquingarayzabal@outlook.com>
2020-11-13 14:11:29 -0300

    forms + img

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,14 +4,63 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g,".");
 const title = 'SportLand'
 
 const productController = {
-    main: function (req, res, next){
-        res.render('Funciona', {items: dataProducts})
-    },
-    add: function (req, res, next){
+    create: function (req, res, next){
         res.render('addProduct', {title})
     },
-    carrito: function (req, res, next){
-        res.render('carrito', {title})
+    store : function(req, res, next){
+        dataProducts.push(req.body);
+        let dataProductsJSON = JSON.stringify(dataProducts);
+        fs.writeFileSync(__dirname + "/../database/listado.json", dataProductsJSON);
+        res.send("Producto Creado")
+    },
+    edit : function(req, res, next){
+        var idProduct = req.params.id;
+        var idFound;
+        for(var i=0;i < dataProducts.length;i++){
+            if(dataProducts[i].id == idProduct){
+                idFound = dataProducts[i];
+                break;
+            }
+        }
+        if(idFound){
+            res.render("editProductFrom",{idFound})
+        }else{
+            res.send("Producto no encontrado");
+        }
+    },
+    update : function(req, res, next){
+        var idProduct = req.params.id;
+        var editProducts = dataProducts.map(function(product){
+            if(product.id == idProduct){
+                let editProduct = req.body;
+                editProduct.id = idProduct;
+                return editProduct;
+            }
+            return user;
+        });
+        editProductsJSON = JSON.stringify(editProducts);
+        fs.writeFileSync(__dirname + "/../database/listado.json",editProductsJSON);
+        res.redirect("/products/edit/" + idProduct);
+    },
+    delete : function(req,res,next){
+        var idProduct = req.params.id;
+        var idFound;
+        for(var i=0;i < dataProducts.length;i++){
+            if(dataProducts[i].id == idProduct){
+                idFound = dataProducts[i];
+                break;
+            }
+        }
+        if(idFound){
+            var productDestroy = dataProducts.filter(function(product){
+                return product.id != idProduct;
+            });
+            productDestroyJSON = JSON.stringify(productDestroy);
+            fs.writeFileSync(__dirname + "/../database/listado.json",productDestroyJSON);
+            res.send("Producto eliminado")
+        }else{
+            res.send("Producto no encontrado");
+        }
     },
     ver:function(req,res,next){
         var productId = req.params.id;
@@ -29,6 +78,9 @@ const productController = {
         }else{
             res.send("No hay producto con ese id");
         }
+    },
+    carrito: function (req, res, next){
+        res.render('carrito', {title})
     },
     products: function (req, res, next){
         res.render('products', {title})
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var productController = require('../controllers/productController')
+var productController = require('../controllers/productController');
+const path = require('path');
+const multer = require('multer');
+
+/* Multer */
+var storage = multer.diskStorage({
+	destination:(req,file,cb)=>{
+		cb(null,'public/images/products');
+	},
+	filename:(req,file,cb)=>{   
+		cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	}
+});
+var upload = multer({storage:storage});
 
 /* GET products listing. */
 router.get('/', productController.products);
-router.get('/addproduct', productController.add);
-router.get('/carrito', productController.carrito);
+router.get('/carrito', productController.carrito); 
+
+
+router.get('/addproduct',productController.create);
+
+router.post('/addproduct',upload.any(), productController.store);
+
+router.get('/edit/:id',productController.edit);
+router.post('/edit/:id',productController.update);
+
+router.get("/delete/:id",productController.delete);
+
 router.get('/:id', productController.ver);
 
 
