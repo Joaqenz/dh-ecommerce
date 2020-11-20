9b5da35113dc9d1f4798b98aa7df399a2c75fcfb
Joaquin Garayzabal <joaquingarayzabal@outlook.com>
2020-11-19 21:33:31 -0300

    act

--- middlewares/userValidator.js
+++ middlewares/userValidator.js
@@ -1,27 +1,27 @@
 const readUsers = require('../helpers/readUsers')
 const {check, validationResult, body} = require('express-validator');
-let users = readUsers;
+let users = readUsers();
 
 const userValidator = [
     body('username')
     .notEmpty().withMessage('Completa el usuario.').bail().custom((value) => {
         for(var i=0;i < users.length;i++){
-            if(value  == users[i].name){
-              return false;
+            if(value  !== users[i].username){
+              return true;
             }
-            return true;
+            return false;
         }
     }).withMessage('Ese usuario ya está en uso.'),
     body('firstname').isLength({ min: 2 }).withMessage('El nombre requiere minimo 2 caracteres.'),
     body('lastname').notEmpty().withMessage('Completa tu Apellido'),
     body('cel').notEmpty().withMessage('Completa el telefono.'),
-    body('email').isEmail().withMessage('No es un email.')
+    body('email').isEmail().withMessage('No es un email.').bail()
     .custom((value) => {
       for(var i=0;i < users.length;i++){
-          if(value  == users[i].email){
-            return false;
+          if(value  !== users[i].email){
+            return true;
           }
-          return true;
+          return false;
       }
     }).withMessage('Ese email ya está en uso.'),
     body('password')
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/userController')
-var userValidator = require('../middlewares/userValidator')
+const userController = require('../controllers/userController')
+const userValidator = require('../middlewares/userValidator')
 
 /* GET users listing. */
 router.get('/', userController.main);
