fe73c75dcc20a8a55e38ae78e5fc8f98f1cb0107
Joaquin Garayzabal <joaquingarayzabal@outlook.com>
2021-01-05 12:53:55 -0300

    mac

--- database/config/config.js
+++ database/config/config.js
@@ -1,21 +1,22 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "sportland_db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 8889
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
