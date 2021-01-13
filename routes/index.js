357aa8b69a52530ceeb536b725b0c1611c8d2e66
Joaquin <joaquingarayzabal@outlook.com>
2021-01-13 03:56:34 -0300

    carousel ok e inicio de filtros

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'SportLand', req });
 });
 router.get('/template', function(req, res, next) {
-    res.render('template', { title: 'SportLand' });
+    res.render('template', { req, title: 'SportLand' });
 });
 
 module.exports = router;
